416693c076905212010b5689e739cf622a64c30d
侯景业 <houjingye@houkeis-MacBook-puro.local>
2018-03-11 19:24:27 -0400

    changed

--- play-java-starter-example-2.6.x/test/FunctionalTest.java
+++ play-java-starter-example-2.6.x/test/FunctionalTest.java
@@ -13,11 +13,7 @@ public class FunctionalTest extends WithApplication {
 
     @Test
     public void renderTemplate() {
-        // If you are calling out to Assets, then you must instantiate an application
-        // because it makes use of assets metadata that is configured from
-        // the application.
-
-        Content html = views.html.index.render("Your new application is ready.");
+        Content html = views.html.index.render();
         assertThat("text/html").isEqualTo(html.contentType());
         assertThat(html.body()).contains("Your new application is ready.");
     }
