2eb3baf3668479e0ae28d9dc765aefed06496aaa
侯景业 <houjingye@houkeisbookpuro.wireless.concordia.ca>
2018-03-12 15:41:59 -0400

    second version

--- play-java-starter-example-2.6.x/test/FunctionalTest.java
+++ play-java-starter-example-2.6.x/test/FunctionalTest.java
@@ -1,8 +1,17 @@
 import org.junit.Test;
 import play.test.WithApplication;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static play.mvc.Http.Status.OK;
+import static play.test.Helpers.*;
+import org.junit.Test;
+
+import play.mvc.Result;
 import play.twirl.api.Content;
 
-import static org.assertj.core.api.Assertions.assertThat;
+
+
 
 /**
  * A functional test starts a Play application for every test.
@@ -11,10 +20,12 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 public class FunctionalTest extends WithApplication {
 
-    @Test
-    public void renderTemplate() {
-        Content html = views.html.index.render();
-        assertThat("text/html").isEqualTo(html.contentType());
-        assertThat(html.body()).contains("Your new application is ready.");
-    }
+//    @Test
+//    public void renderTemplate() {
+//        Content html = views.html.index.render();
+//        assertThat("text/html").isEqualTo(html.contentType());
+//        assertThat(html.body()).contains("Your new application is ready.");
+//    }
+
+
 }
