eb315218810e7b25f5be549dbcf29bfcdf5d1a11
UNO998 <shenyiran2015@outlook.com>
2018-03-12 10:58:06 -0400

    整理dev-hjy分支

---
+++ app/models/Twitter.java
@@ -0,0 +1,12 @@
+package models;
+
+public class Twitter {
+    public String hashtag;
+
+    public Twitter() {
+    }
+
+    public Twitter(String hashtag) {
+        this.hashtag = hashtag;
+    }
+}
