39952397426c84d5f61e8f10dd1a0216acd94546
侯景业 <houjingye@houkeis-MacBook-puro.local>
2018-03-12 23:41:50 -0400

    abc

--- play-java-starter-example-2.6.x/test/FunctionalTest.java
+++ play-java-starter-example-2.6.x/test/FunctionalTest.java
@@ -1,31 +1,22 @@
 import org.junit.Test;
-import play.test.WithApplication;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static play.mvc.Http.Status.OK;
-import static play.test.Helpers.*;
-import org.junit.Test;
-
+import play.mvc.Http.RequestBuilder;
 import play.mvc.Result;
-import play.twirl.api.Content;
+import play.test.Helpers;
 
+import static junit.framework.TestCase.assertEquals;
+import static play.mvc.Http.HttpVerbs.GET;
+import static play.shaded.ahc.io.netty.handler.codec.rtsp.RtspResponseStatuses.NOT_FOUND;
+import static play.test.Helpers.route;
 
+public class FunctionalTest {
 
+    @Test
+    public void testBadRoute() {
+        RequestBuilder request = Helpers.fakeRequest()
+                .method(GET)
+                .uri("/twitter/getPage");
 
-/**
- * A functional test starts a Play application for every test.
- *
- * https://www.playframework.com/documentation/latest/JavaFunctionalTest
- */
-public class FunctionalTest extends WithApplication {
-
-//    @Test
-//    public void renderTemplate() {
-//        Content html = views.html.index.render();
-//        assertThat("text/html").isEqualTo(html.contentType());
-//        assertThat(html.body()).contains("Your new application is ready.");
-//    }
-
-
+        Result result = route(app, request);
+        assertEquals(NOT_FOUND, result.status());
+    }
 }
